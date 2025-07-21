72cc27cea2c4b45ba737f7e9b0377f21f5449bb5
auraor <6505672+auraor@user.noreply.gitee.com>
2025-07-21 14:32:56 +0800

    异步执行事件配置

---
+++ im-platform/src/main/java/xyz/qy/implatform/config/AsyncEventConfig.java
@@ -0,0 +1,17 @@
+package xyz.qy.implatform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+@Configuration
+public class AsyncEventConfig {
+    @Bean
+    public ApplicationEventMulticaster eventMulticaster() {
+        SimpleApplicationEventMulticaster multicaster = new SimpleApplicationEventMulticaster();
+        multicaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        return multicaster;
+    }
+}
